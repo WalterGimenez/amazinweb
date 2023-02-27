4f01bfd277967a932a04f3284cb5ad25dcfbeb1f
WalterGimenez <wallegimenez@gmail.com>
2023-02-27 12:35:45 -0300

    added showData for the cards

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,21 @@
 const cardList = document.getElementById('list-card')
 
-for (const dataCard of data.events) {
-    cardList.innerHTML += `<div class="card cardmarg cardsize">
-    <img src="${dataCard.image}" class="card-img-top imgcard" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">${dataCard.name}</h5>
-        <p class="card-text">${dataCard.description}</p>
-    </div>
-    <div class="card-body">
-        <p class="pinline">Price $${dataCard.price}</p>
-        <a href="./detail.html" class="btn btn-primary">More info..</a>
-    </div>
-</div>`
+let dataEvents =  data.events
+
+function showData(){
+    for (const dataCard of dataEvents) {
+        cardList.innerHTML += `<div class="card cardmarg cardsize">
+        <img src="${dataCard.image}" class="card-img-top imgcard" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${dataCard.name}</h5>
+            <p class="card-text">${dataCard.description}</p>
+        </div>
+        <div class="card-body">
+            <p class="pinline">Price $${dataCard.price}</p>
+            <a href="./detail.html" class="btn btn-primary">More info..</a>
+        </div>
+    </div>`
+    }
 }
+
+showData()
