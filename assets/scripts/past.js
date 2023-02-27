6bb4438af7d3c8bdd919954e4d0cb467aec1e5ae
WalterGimenez <wallegimenez@gmail.com>
2023-02-27 18:20:16 -0300

    fixed images's cards

--- assets/scripts/past.js
+++ assets/scripts/past.js
@@ -5,13 +5,13 @@ for (const dataCard of data.events) {
         cardListPast.innerHTML += `<div class="card cardmarg cardsize">
         <img src="${dataCard.image}" class="card-img-top imgcard" alt="...">
         <div class="card-body">
-            <h5 class="card-title">${dataCard.name}</h5>
-            <p class="card-text">${dataCard.description}</p>
-        </div>
-        <div class="card-body">
-            <p class="pinline">Price $${dataCard.price}</p>
-            <a href="./detail.html" class="btn btn-primary">More info..</a>
-        </div>
+        <h5 class="card-title">${dataCard.name}</h5>
+        <p class="card-text">${dataCard.description}</p>
+    </div>
+    <div class="card-footer">
+    <p class="pinline">Price $${dataCard.price}</p>
+        <a href="./detail.html" class="btn btn-primary">More info..</a>
+    </div>
     </div>`
     }
 }
