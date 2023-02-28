5897f1b2b492221cd5ef23d6268134ae5843c933
WalterGimenez <wallegimenez@gmail.com>
2023-02-28 11:45:30 -0300

    created a function and used forEach() in each js file

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,20 @@
 const cardList = document.getElementById('list-card')
 
-data.events.forEach(event => {
-    cardList.innerHTML +=
-    `<div class="card cardmarg cardsize">
-        <img src="${event.image}" class="card-img-top imgcard" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${event.name}</h5>
-            <p class="card-text">${event.description}</p>
-        </div>
-        <div class="card-footer">
-        <p class="pinline">Price $${event.price}</p>
-            <a href="./detail.html" class="btn btn-primary">More info..</a>
-        </div>
-    </div>`
-});
+function allCards(data){
+    data.events.forEach(event => {
+        cardList.innerHTML +=
+        `<div class="card cardmarg cardsize">
+            <img src="${event.image}" class="card-img-top imgcard" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${event.name}</h5>
+                <p class="card-text">${event.description}</p>
+            </div>
+            <div class="card-footer">
+            <p class="pinline">Price $${event.price}</p>
+                <a href="./detail.html" class="btn btn-primary">More info..</a>
+            </div>
+        </div>`
+    });
+}
+
+allCards(data)
--- assets/scripts/past.js
+++ assets/scripts/past.js
@@ -1,18 +1,22 @@
 const cardListPast = document.getElementById('list-card')
 
-data.events.forEach(event => {
-    if(data.currentDate > event.date){
-        cardListPast.innerHTML +=
-    `<div class="card cardmarg cardsize">
-        <img src="${event.image}" class="card-img-top imgcard" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${event.name}</h5>
-            <p class="card-text">${event.description}</p>
-        </div>
-        <div class="card-footer">
-        <p class="pinline">Price $${event.price}</p>
-            <a href="./detail.html" class="btn btn-primary">More info..</a>
-        </div>
-    </div>`
-    }
-});
+function cardsPast(data){
+    data.events.forEach(event => {
+        if(data.currentDate > event.date){
+            cardListPast.innerHTML +=
+        `<div class="card cardmarg cardsize">
+            <img src="${event.image}" class="card-img-top imgcard" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${event.name}</h5>
+                <p class="card-text">${event.description}</p>
+            </div>
+            <div class="card-footer">
+            <p class="pinline">Price $${event.price}</p>
+                <a href="./detail.html" class="btn btn-primary">More info..</a>
+            </div>
+        </div>`
+        }
+    });
+}
+
+cardsPast(data)
